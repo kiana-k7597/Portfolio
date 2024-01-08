3c0893bd9b76ece301e1e0e4ec9dfca60c7c22b7
kiana-k7597 <kiana.k7597@gmail.com>
2024-01-08 19:20:47 +0000

    Change styling to styled-components, imrpovements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-
+import { Header, HeaderText } from "./styling/Header.styles.tsx";
+import { Body, BodyText } from "./styling/Body.styles.tsx";
+import { MainPage } from "./styling/MainPage.styles.tsx";
 function App() {
   return (
-    <div className="App">
-      <div className="body">
-        <p>Website Under Construction...</p>
-      </div>
-    </div>
+    <>
+      <MainPage>
+        <Header>
+          <HeaderText>Kiana Kabiri</HeaderText>
+          <HeaderText>Software Engineer</HeaderText>
+        </Header>
+        <Body>
+          <BodyText>THG - 2020-present</BodyText>
+          <BodyText>University of Manchester - BEng(Hons)- 2017-2020</BodyText>
+        </Body>
+      </MainPage>
+    </>
   );
 }
 
---
+++ src/styling/Body.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+display: flex;
+flex-direction: column;
+min-height: 100vh;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+margin-top: -100px;
+`
+
+export const BodyText = styled.div`
+font-size: calc(10px + 2vmin);
+margin: 10px;
+`
