26c83475aa49375e93fb2964ff810319316e7fad
kiana-k7597 <kiana.k7597@gmail.com>
2024-01-09 01:38:27 +0000

    Style header

--- src/styling/Header.styles.tsx
+++ src/styling/Header.styles.tsx
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const Header =  styled.header`
+font-family: Verdana;
 display: flex;
 flex-direction: column;
 justify-content: center; 
+padding-left: 70px;
 `
 
 export const Title = styled.h1`
 display: flex;
 justify-content: flex-start;
-padding-left: 30px;
-font-size: calc(15px + 2vmin);
+font-size: calc(30px + 2vmin);
 `
 
 export const Subtext = styled.h2`
 display: flex;
 justify-content: flex-start;
-padding-left: 30px;
-font-size: calc(5px + 2vmin);
+font-size: calc(10px + 2vmin);
 `
