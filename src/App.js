fe18c5aa866c57dc6467a15accc3041148e9ee65
kiana-k7597 <kiana.k7597@gmail.com>
2024-01-09 01:36:18 +0000

    Improve structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Header, Title } from "./styling/Header.styles.tsx";
+import { Header, Title, Subtext } from "./styling/Header.styles.tsx";
 import { Body, BodyText } from "./styling/Body.styles.tsx";
 import { MainPage } from "./styling/MainPage.styles.tsx";
 function App() {
@@ -7,11 +7,11 @@ function App() {
       <MainPage>
         <Header>
           <Title>Kiana Kabiri</Title>
-          <Title>Software Engineer</Title>
+          <Subtext>Software Engineer</Subtext>
         </Header>
         <Body>
           <BodyText>THG - 2020-present</BodyText>
-          <BodyText>University of Manchester - BEng(Hons)- 2017-2020</BodyText>
+          <BodyText>University of Manchester - BEng(Hons) - 2017-2020</BodyText>
         </Body>
       </MainPage>
     </>
--- src/styling/Header.styles.tsx
+++ src/styling/Header.styles.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
 export const Header =  styled.header`
-font-family: Verdana;
-font-size: calc(20px + 2vmin);
 display: flex;
 flex-direction: column;
 justify-content: center; 
@@ -13,4 +11,11 @@ display: flex;
 justify-content: flex-start;
 padding-left: 30px;
 font-size: calc(15px + 2vmin);
+`
+
+export const Subtext = styled.h2`
+display: flex;
+justify-content: flex-start;
+padding-left: 30px;
+font-size: calc(5px + 2vmin);
 `
